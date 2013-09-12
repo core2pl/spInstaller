e5af3c4b4bb7846e4f4e7dc58aabb7e0e42e8bf9
marian <marian@marian-GA-MA78GPM-DS2H.(none)>
2013-09-12 20:37:16 +0200

    echo removed

--- install.php
+++ install.php
@@ -7,15 +7,15 @@
  * To change this template use File | Settings | File Templates.
  */
 
-echo exec('git clone http://github.com/core2pl/sp.git engine/');
+exec('git clone http://github.com/core2pl/sp.git engine/');
 exec('git cd engine/ && git checkout master && cd ../');
 
-echo exec('git clone http://github.com/fabpot/Twig.git Twig/');
+exec('git clone http://github.com/fabpot/Twig.git Twig/');
 
-echo exec('git clone http://github.com/ckeditor/ckeditor-releases.git ckeditor/');
-echo exec('cd ckeditor/ && git checkout stable/full && cd ../');
+exec('git clone http://github.com/ckeditor/ckeditor-releases.git ckeditor/');
+exec('cd ckeditor/ && git checkout stable/full && cd ../');
 
-echo exec('git clone http://github.com/core2pl/theme_default.git themes/default/');
+exec('git clone http://github.com/core2pl/theme_default.git themes/default/');
 //exec('git cd theme/ && git checkout master && cd ../'); TODO checkout to branch master
 
 header('Location: /installer/welcome');
